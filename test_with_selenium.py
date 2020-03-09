474e4d17cc38ac0ec9658dfcf9ce46406d3bfe97
unknown <leodrl9@gmail.com>
2020-03-08 22:26:09 -0300

    first commit

---
+++ test_with_selenium.py
@@ -0,0 +1,31 @@
+"""
+A simple selenium test example written by python
+"""
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# download from http://chromedriver.storage.googleapis.com/index.html?path=2.14/
+CHROME_PATH = "C:\\Users\\Dell\\Downloads\\chromedriver.exe"
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            executable_path=CHROME_PATH
+            )
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+if __name__ == "__main__":
+    unittest.main()
